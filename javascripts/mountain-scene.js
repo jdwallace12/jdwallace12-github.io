0146668d7e90ecc0612e54a6476fd4adb5ef6515
John Wallace <jwallace@appneta.com>
2015-08-31 23:06:04 -0400

    skier back in no path

--- javascripts/mountain-scene.js
+++ javascripts/mountain-scene.js
@@ -1,32 +1,49 @@
 $(document).ready(function() {
     var skipath = {
-      entry : {
-        curviness: 1.25,
-        autoRotate: true,
-        values: [
-            {x: 100,  y: 0}
-          ]
-      },
-      leave : {
-        curviness: 1.25,
-        autoRotate: true,
-        values: [
-            {x: $(window).width() + 300,  y: 100}
-          ]
-      }
+        entry: {
+            curviness: 1.25,
+            autoRotate: true,
+            values: [{
+                x: 100,
+                y: 0
+            }]
+        },
+        leave: {
+            curviness: 1.25,
+            autoRotate: true,
+            values: [{
+                x: $(window).width() + 300,
+                y: 0
+            }]
+        }
     };
     // init controller
     var controller = new ScrollMagic.Controller();
 
     // create tween
     var tween = new TimelineMax()
-      .add(TweenMax.to($("#skier"), 1.2, {css:{bezier:skipath.entry}, ease:Power1.easeInOut}))
-      .add(TweenMax.to($("#skier"), 1, {css:{bezier:skipath.leave}, ease:Power1.easeInOut}));
+        .add(TweenMax.to($("#skier"), 1.2, {
+            css: {
+                bezier: skipath.entry
+            },
+            ease: Power1.easeInOut
+        }))
+        .add(TweenMax.to($("#skier"), 1, {
+            css: {
+                bezier: skipath.leave
+            },
+            ease: Power1.easeInOut
+        }));
 
     // build scene
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 1000, offset: 0})
-            .setPin("#target")
-            .setTween(tween)
-            // .addIndicators() // add indicators (requires plugin)
-            .addTo(controller);
-  });
+    var scene = new ScrollMagic.Scene({
+            triggerElement: "#trigger",
+            duration: 500,
+            offset: 0
+        })
+        .setPin("#target")
+        .setTween(tween)
+        // .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+
+});
